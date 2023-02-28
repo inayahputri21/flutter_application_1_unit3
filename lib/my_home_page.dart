c7563490ded39ae8fc04b68aa5c27dbec470ad10
inayahputri <inayahputri21>
2023-02-28 15:34:56 +0700

    belajar column, row dan button

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -13,21 +13,47 @@ class MyHomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 250, 127, 168),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Color.fromARGB(255, 143, 235, 146),
-        child: Center(child: const Text(
-          "Hello Girls", 
-          style: TextStyle(
-            fontSize: 50, 
-            color: Colors.red,
-            decoration: TextDecoration.underline,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
+      body: Column(
+        children: [
+      Flexible(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width,
+              color: Color.fromARGB(255, 143, 235, 146),
+              child: Center(child: const Text(
+                "Hello Girls", 
+                style: TextStyle(
+                  fontSize: 50, 
+                  color: Colors.red,
+                  decoration: TextDecoration.underline,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+          
+                ))
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            height: 50,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: ElevatedButton (
+              onPressed: () {},
+            child: Text ("Tekan"),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),
 
-          ))
-        ),
+              ),
+              elevation: 3,
+              shadowColor: Colors.black,
+              
+                ) 
+            ),
+
+            ),
+          
+        ],
       ), 
     );
   }
